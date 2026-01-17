db4e9daf8733f1dbab31a6191fdc6c75e13cbd22
salsabilarh <salsabilarafifahh@student.ce.undip.ac.id>
2026-01-17 15:45:53 +0700

    fix limit access

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -12,7 +12,7 @@ const likes = require('../../Interfaces/http/api/likes');
 const createServer = async (container) => {
   const server = Hapi.server({
     host: '0.0.0.0',
-    port: process.env.PORT || 3000,
+    port: 3000,
     routes: {
       cors: {
         origin: ['*'],
